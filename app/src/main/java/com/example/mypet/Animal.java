fc2b162fe6e20a8f535d5594ecc26105de2b3f58
Braian-Pimentao <braian.pimentao@gmail.com>
2021-04-24 17:59:43 +0200

    Creacion de la lista de anuncios

---
+++ app/src/main/java/com/example/mypet/Animal.java
@@ -0,0 +1,9 @@
+package com.example.mypet;
+
+public class Animal {
+    private String nombre;
+    private String tipo;
+    private String raza;
+    private int foto;
+    private int  sexo;
+}
--- app/src/main/java/com/example/mypet/MainActivity.java
+++ app/src/main/java/com/example/mypet/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.mypet;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> listaDatos;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.lista_anuncios);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        listaDatos = new ArrayList<>();
+
+        for (int i = 0; i <=50 ; i++) {
+            listaDatos.add("Dato: " +i);
+        }
+
+        AdaptadorDatos adaptadorDatos = new AdaptadorDatos(listaDatos);
+        recyclerView.setAdapter(adaptadorDatos);
     }
 }
