b350139aa138948889e2bc656159b8ad65dab2e2
Braian-Pimentao <braian.pimentao@gmail.com>
2021-07-09 15:50:55 +0200

    Token a kotlin

---
+++ app/src/main/java/com/aplicacion/mypet/models/Token.kt
@@ -0,0 +1,12 @@
+package com.aplicacion.mypet.models
+
+class Token {
+    var token: String? = null
+
+    constructor()
+
+    constructor(token: String?) {
+        this.token = token
+    }
+
+}
